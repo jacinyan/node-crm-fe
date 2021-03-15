c55b328204d2c05969d83be35bfed8544b88b5e5
Jacin <jacinjiyan@gmail.com>
2021-03-15 15:46:37 +1100

    missed require

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'production',
